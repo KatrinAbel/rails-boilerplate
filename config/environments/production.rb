e9b20a869f4761dd6f0b2d2476a2db8336989d9e
Katrin Abel <katrin.abel@archipinion.com>
2019-06-22 18:07:34 +0200

    Add domain name to sendgrid setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "http://rails-boilerplate-animals.herokuapp.com/" }
 end
