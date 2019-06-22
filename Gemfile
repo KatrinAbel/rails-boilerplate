a2508dd063467dcb1521838e69aaca984272d2fc
Katrin Abel <katrin.abel@archipinion.com>
2019-06-22 16:07:08 +0200

    Switch from sqlite to postgres

--- Gemfile
+++ Gemfile
@@ -8,11 +8,14 @@ gem "twitter-bootstrap-rails"
 gem 'carrierwave'
 gem 'devise'
 gem 'cancancan'
+gem 'sidekiq'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
+# Use postgresSQL as the database for Active Record
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_12_153542) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -19,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_06_12_153542) do
     t.string "attachement"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -36,4 +39,5 @@ ActiveRecord::Schema.define(version: 2019_06_12_153542) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
